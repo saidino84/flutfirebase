c52c7b7737e357d6f1333b696a8d4d587b939ebf
saidino84 <tylorguy2018@gmail.com>
2021-07-19 12:21:24 -0400

    SHOWWW DE BOLA FIRESTORE INTEGRADO COM SUCESSO POSSO FAZER QUALQUER ACAO AGORA DE CRUDE COM FIREBASEFIRESTORE

--- lib/app/ui/pages/home_page/home_page.dart
+++ lib/app/ui/pages/home_page/home_page.dart
@@ -7,6 +7,7 @@ import '../../../controllers/home_controller.dart';
 class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
+    var primaryColor = Theme.of(context).primaryColor;
     return Scaffold(
       appBar: AppBar(
         title: TextField(
@@ -25,11 +26,64 @@ class HomePage extends GetView<HomeController> {
             // como ja criei refencia d todo no homeController entao posso usalo
             stream: controller.todoReference.orderBy('title').snapshots(),
             builder: (ctx, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData)
+                return Center(child: CircularProgressIndicator.adaptive());
               return ListView(
                 children: snapshot.data!.docs.map((todo) {
-                  return Center(
+                  return Dismissible(
+                    background: Container(
+                      alignment: Alignment.centerLeft,
+                      decoration: BoxDecoration(color: primaryColor),
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.grey[800],
+                      ),
+                    ),
+                    key: Key("${todo['title']}"),
+                    onDismissed: (direction) {
+                      controller.todoReference.doc(todo.id).delete();
+                      Get.snackbar('Delecao', 'Deletado com sucesso',
+                          backgroundColor: Colors.white);
+                    },
                     child: ListTile(
-                      title: Text(todo['title']),
+                      title: Text(
+                        "${todo['title']} + ${todo['checked']}",
+                        style: TextStyle(color: Colors.white30),
+                      ),
+                      leading: Checkbox(
+                        value:
+                            todo['checked'].toString().trim().toLowerCase() ==
+                                    'true'
+                                ? true
+                                : false,
+                        // == 'false'? true : false,
+                        //parse(todo['checked']),
+                        onChanged: (v) {
+                          bool todo_checked =
+                              todo['checked'].toString().trim().toLowerCase() ==
+                                      'true'
+                                  ? true
+                                  : false;
+                          controller.todoReference
+                              .doc(todo.id)
+                              .update({'checked': !todo_checked})
+                              .then(
+                                (value) => Get.snackbar(
+                                  'Updating',
+                                  'check updated sucessfull',
+                                  backgroundColor: Colors.blueAccent,
+                                ),
+                              )
+                              .catchError((error) {
+                                Get.snackbar('Update',
+                                    'Erro ao fazer update por favor verifique o database',
+                                    backgroundColor: primaryColor);
+                              });
+                        },
+                      ),
+                      onLongPress: () {
+                        // Apage ao precionar com alta preesao
+                      },
                     ),
                   );
                 }).toList(),
@@ -38,7 +92,7 @@ class HomePage extends GetView<HomeController> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          if (controller.titleEditctl.text != null) {
+          if (controller.titleEditctl.text.isNotEmpty) {
             controller.todoReference.add({
               'title': controller.titleEditctl.text,
               'checked': false,
